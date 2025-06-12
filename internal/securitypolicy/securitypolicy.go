039bd6ff277499dfcea6cf06a74efc215b8454c8
Matias Charriere <matias@giantswarm.io>
2025-06-12 10:21:35 +0200

    cleanup
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- internal/securitypolicy/securitypolicy.go
+++ internal/securitypolicy/securitypolicy.go
@@ -42,31 +42,3 @@ func (r *SecurityPolicy) WithHTTPRoute(httpRoute *gatewayv1.HTTPRoute) *Security
 func (r *SecurityPolicy) WithAnnotations() *SecurityPolicy {
 	return r
 }
-
-// // Setup HTTPRoute values from an Ingress resource
-// func (r *HTTPRoute) WithIngress(ingress *networkingv1.Ingress) *HTTPRoute {
-// 	r.Ingress = ingress
-// 	r.Resource.ObjectMeta.Name = ingress.Name
-// 	r.Resource.ObjectMeta.Namespace = ingress.Namespace
-//
-// 	r.Resource.Spec.Hostnames = []gatewayv1.Hostname{gatewayv1.Hostname(ingress.Spec.Rules[0].Host)}
-//
-// 	rules := toRules(ingress.Spec.Rules)
-// 	r.Resource.Spec.Rules = rules
-//
-// 	return r
-// }
-//
-// func (r *HTTPRoute) WithGateway(name, namespace, sectionName string) *HTTPRoute {
-// 	r.Resource.Spec.ParentRefs = []gatewayv1.ParentReference{
-// 		{
-// 			Name:      gatewayv1.ObjectName(name),
-// 			Namespace: (*gatewayv1.Namespace)(&namespace),
-// 		},
-// 	}
-// 	if sectionName != "" {
-// 		r.Resource.Spec.ParentRefs[0].SectionName = (*gatewayv1.SectionName)(&sectionName)
-// 	}
-//
-// 	return r
-// }
